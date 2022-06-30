0d05fd15fbb2f1113e4168f612730cd57108fc61
Alla Shkolnik <5106275+December11@users.noreply.github.com>
2022-07-01 01:42:36 +0300

    добавила смену цвета по тапу на экран

---
+++ Task2/Task2/Extensions/UIColor.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor.swift
+//  Task2
+//
+//  Created by Alla Shkolnik on 01.07.2022.
+//
+
+import UIKit
+
+extension UIColor {
+    static var randomColor: UIColor {
+        let color = UIColor(
+            red: CGFloat.random(in: 155...255) / 255,
+            green: CGFloat.random(in: 155...255) / 255,
+            blue: CGFloat.random(in: 155...255) / 255,
+            alpha: 1.0)
+        return color
+    }
+}
--- Task2/Task2/Presentation/Color/ViewControllers/ColorViewController.swift
+++ Task2/Task2/Presentation/Color/ViewControllers/ColorViewController.swift
@@ -9,10 +9,36 @@ import UIKit
 
 final class ColorViewController: UIViewController {
     
+    let label = UILabel()
+    var tapGesture = UITapGestureRecognizer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemYellow
+        configurateTapGesture()
+        configurateLabel()
+        view.backgroundColor = UIColor.randomColor
     }
     
+    private func configurateTapGesture() {
+        tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapHandler(_:)))
+        tapGesture.numberOfTapsRequired = 1
+        tapGesture.numberOfTouchesRequired = 1
+        view.addGestureRecognizer(tapGesture)
+        view.isUserInteractionEnabled = true
+    }
     
+    private func configurateLabel() {
+        label.textColor = UIColor.black
+        label.font = UIFont.systemFont(ofSize: 24, weight: .medium)
+        label.text = "TAP"
+        view.addSubview(label)
+        label.snp.makeConstraints { make in
+            make.centerY.equalTo(view.snp.centerY)
+            make.centerX.equalTo(view.snp.centerX)
+        }
+    }
+    
+    @objc private func tapHandler(_ sender: UITapGestureRecognizer) {
+        view.backgroundColor = UIColor.randomColor
+    }
 }
